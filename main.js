5c06433be7ed1a827ed3b24e797728d7a8a7dffe
Kevin Dahlhoff <kdahlhoff3@gmail.com>
2024-02-20 14:12:15 +0100

    refactor

--- main.js
+++ main.js
@@ -13,71 +13,111 @@ function returnCurrentDate() {
   return [todoDate, todoTime]
 }
 
-function createNewTodoElement() {
-  var initialHeadlineForTodo = document.getElementById("initialTodoHeadLine");
-  if (initialHeadlineForTodo) {
-    initialHeadlineForTodo.remove();
-  }
-
-  todoCounter += 1;
-
-  //create a tag
+function createOuterTodoContainer() {
   var outerTodoContainer = document.createElement("a");
+  outerTodoContainer.id = "todo_" + todoCounter;
   outerTodoContainer.href = "#";
   outerTodoContainer.classList.add("x");
   outerTodoContainer.className = "list-group-item list-group-item-action";
   outerTodoContainer.setAttribute("aria-current", "true");
+  // outerTodoContainer.addEventListener("click", () => {
+  //   alert("This is a test");
+  // });
+  return outerTodoContainer;
+}
 
-  //create div for date
+function createDateContainer() {
   var divForDate = document.createElement("div");
   divForDate.classList.add("d-flex", "w-100", "justify-content-between");
-  outerTodoContainer.appendChild(divForDate);
+  return divForDate;
+}
 
-  //create date
+function createDate() {
   var date = document.createElement("h5");
   date.classList.add("mb-1");
   date.innerHTML = returnCurrentDate()[0];
-  divForDate.appendChild(date);
+  return date;
+}
 
-  //create time
+function createTime() {
   var time = document.createElement("small");
   time.innerHTML = returnCurrentDate()[1];
-  divForDate.appendChild(time);
+  return time;
+}
 
-  //create p todo headline
+function createTodo() {
   var todo = document.createElement("p");
   todo.classList.add("mb-1");
   todo.innerHTML = document.getElementById("todoInput").value;
-  outerTodoContainer.appendChild(todo);
+  return todo;
+}
 
-  //create div for description
+function createDescriptionContainer() {
   var divForDescription = document.createElement("div");
   divForDescription.classList.add("mb-3");
   divForDescription.style = "margin-top:10px!important;";
-  outerTodoContainer.appendChild(divForDescription);
+  return divForDescription;
+}
 
-  //create description
+function createDescription() {
   var description = document.createElement("textarea");
   description.id = "todo_" + todoCounter + "_description";
   description.classList.add("form-control");
   description.style = "background-color: #212529;";
   description.disabled = true;
   description.innerHTML = document.getElementById("todoDescription").value;
-  divForDescription.appendChild(description);
+  return description;
+}
 
-  //create close button
+function createCloseButton() {
   var closeTodoBtn = document.createElement("button");
   closeTodoBtn.type = "button";
   closeTodoBtn.classList.add("btn", "btn-primary");
-  closeTodoBtn.style = "--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; margin-top: 10px!important;";
+  closeTodoBtn.style = "--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;";
   closeTodoBtn.innerHTML = "Close Todo";
+  return closeTodoBtn;
+}
 
-  closeTodoBtn.addEventListener("click", () => {
-    outerTodoContainer.remove()
+function createNewTodoElement() {
+  var initialHeadlineForTodo = document.getElementById("initialTodoHeadLine");
+  if (initialHeadlineForTodo) initialHeadlineForTodo.remove();
+
+  todoCounter += 1;
+
+  var outerTodoContainer = createOuterTodoContainer();
+
+  var dateTimeContainer = createDateContainer();
+  var date = createDate();
+  var time = createTime();
+  dateTimeContainer.appendChild(date);
+  dateTimeContainer.appendChild(time);
+  outerTodoContainer.appendChild(dateTimeContainer);
+
+  var todo = createTodo();
+  outerTodoContainer.appendChild(todo);
+
+  var descriptionContainer = createDescriptionContainer();
+  var description = createDescription();
+  descriptionContainer.appendChild(description);
+  outerTodoContainer.appendChild(descriptionContainer);
+
+  var closeButton = createCloseButton();
+  closeButton.addEventListener("click", () => {
+    outerTodoContainer.remove();
+    var x = document.getElementById("list-group").childNodes.length;
+    if (x == 2) {
+      var initialTodoHeadLine = document.createElement("div");
+      initialTodoHeadLine.id = "initialTodoHeadLine";
+
+      var todoHeadline = document.createElement("h4");
+      todoHeadline.innerHTML = "Add Todos to be displayed!";
+
+      initialTodoHeadLine.appendChild(todoHeadline);
+      document.getElementById("list-group").appendChild(initialTodoHeadLine);
+    }
   });
-
-  outerTodoContainer.appendChild(closeTodoBtn);
-
+  outerTodoContainer.appendChild(closeButton);
+  
   // add to list
   var todoList = document
     .getElementById("todoList")
